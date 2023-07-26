a4ba814d9d16e46d4dcd65ca99c79b94199a4ac1
Artur Torbogoshev <torbogoshev@yandex.ru>
2023-07-26 22:13:57 +0700

    second variant

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,84 @@
 <script setup>
-import LogoEffect from "./components/LogoEffect.vue";
 import {ref} from "vue";
-import Info from "./components/Info.vue";
-
-const showLogo = ref(true);
-
-setTimeout(() => showLogo.value = false, 3300);
+import Fade from "./components/Fade.vue";
 
 </script>
 
 <template>
-    <Transition name="logo">
-        <LogoEffect v-if="showLogo"></LogoEffect>
+    <Transition>
+        <Fade :delay="500" :is-fade="true" :on="1000">
+            <img src="./assets/icons/heart.svg" alt="" width="40">
+        </Fade>
     </Transition>
 
-    <Info v-if="!showLogo"></Info>
+    <Transition>
+        <Fade :delay="1800">
+            <div class="address">
+                Чемальский тракт, <span style="font-family: Roboto, serif;">4</span> км
+            </div>
+        </Fade>
+    </Transition>
+
+    <Transition>
+        <Fade :delay="2800">
+            <div class="title">
+                Пора<br>веселиться
+            </div>
+        </Fade>
+    </Transition>
+
+
+    <div class="names-time-container">
+        <Transition>
+            <Fade :delay="4000">
+                <div class="names">
+                    Артабас & Карина<br>женятся
+                </div>
+            </Fade>
+        </Transition>
+
+        <Transition>
+            <Fade :delay="4800">
+                <div class="time">
+                    20 августа 2023<br>в 15:00
+                </div>
+            </Fade>
+        </Transition>
+    </div>
+
 </template>
 
 <style scoped>
-.logo-enter-active,
-.logo-leave-active {
-    transition: opacity 0.5s ease;
+img, .address {
+    position: absolute;
+    top: 10%;
+    right: 10%;
 }
-.logo-enter-from,
-.logo-leave-to {
+
+.title {
+    position: absolute;
+    top: 30%;
+    left: 10%;
+    text-transform: uppercase;
+    font-size: 40px;
+}
+
+.names-time-container {
+    position: absolute;
+    top: 60%;
+    left: 10%;
+    font-size: 25px;
+}
+
+.v-enter-active,
+.v-leave-active {
+    transition: opacity 1s ease;
+    animation: 1s blur;
+    color: transparent;
+}
+
+.v-enter-from,
+.v-leave-to {
     opacity: 0;
 }
 </style>
---
+++ src/components/Fade.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {onMounted, ref} from "vue";
+
+const props = defineProps({
+    delay: Number,
+    on: Number,
+    isFade: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+
+const show = ref(false);
+
+onMounted(() => {
+    setTimeout(() => show.value = true, props.delay);
+    if (props.isFade) {
+        setTimeout(() => show.value = false, props.delay + props.on);
+    }
+});
+
+
+</script>
+
+<template>
+    <div v-if="show">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
